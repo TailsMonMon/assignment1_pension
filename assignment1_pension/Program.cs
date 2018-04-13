993a97bf946188643bc7ae7369ad6c077eb29689
Monica Björk <m.bjork95@gmail.com>
2018-04-13 08:49:01 +0200

    Added a try-catch to the age-input field to prevent a crash.

--- assignment1_pension/Program.cs
+++ assignment1_pension/Program.cs
@@ -8,41 +8,52 @@ namespace assignment1_pension {
     class Program {
         static void Main(string[] args) {
 
+            //      DEFINING SOM DATA
             const int pensionsAlder = 65;   // Age of pension. This makes it easier to edit in case of any changes
+            int alder = 0;  // This will become the person's age once it's enterd
+            string namn = "inget namn än";  // This will become the persons name once it's enterd
 
-            //      Intro
+            //      INTRO
             Console.WriteLine("=== Välkommen ===");
             Console.WriteLine("Här kan du få reda på hur många år som är kvar tills du kan gå i pension.");
             Console.WriteLine();
 
 
-            //      Input
+            //      INPUT
             Console.WriteLine("Först behöver vi erat namn. När ni har skrivit det nedan, tryck på [ENTER] för att bekräfta.");
             Console.Write("Föramn och efternamn: ");
-            string namn = Console.ReadLine();   // Saves the user's name to be used later
+            namn = Console.ReadLine();   // Saves the user's name to be used later
             Console.WriteLine();
 
-            Console.WriteLine("Nu behöver vi veta eran ålder. När ni har skrivit det nedan, tryck på [ENTER] för att bekräfta.");
-            Console.Write("Ålder: ");
-            int alder = Convert.ToInt32(Console.ReadLine());    // Converts and saves the user's age to an integer
+                Console.WriteLine("Nu behöver vi veta eran ålder. När ni har skrivit det nedan, tryck på [ENTER] för att bekräfta.");
+            do {
+                Console.Write("Ålder: ");
+                try {
+                    alder = Convert.ToInt32(Console.ReadLine());    // Converts and saves the user's age to an integer
+                }
+                catch(FormatException) {
+                    Console.WriteLine("Var vänlig och skriv endast siffror (0-9) när ni anger eran ålder.");
+                }
+            } while(alder == 0);
+
             Console.WriteLine();
 
 
-            //      Output
-            if (alder >= 65) {  // 65years and older.
+            //      OUTPUT
+            if (alder >= 65) {  // 65years and older
                 Console.WriteLine("Tack så mycket {0}. Ni kan gå i pension nu.", namn);
             }
             else if (alder >= 55 && alder < 65) {   // 55years and older up to 65
                 Console.WriteLine("Tack så mycket {0}. Ni går snart i pension då det är {1}år kvar.", namn, pensionsAlder - alder);
             }
-            else if (alder <= 20) {     // Younger than 20.
+            else if (alder <= 20) {     // Younger than 20
                 Console.WriteLine("Tack så mycket {0}. Ni har långt kvar tills ni går i pension då det är {1}år kvar.", namn, pensionsAlder - alder);
             }
-            else {  // Between 21 and 54years old.
+            else {  // Between 21 and 54years old
                 Console.WriteLine("Tack så mycket {0}. Ni har {1}år kvar tills pension.", namn, pensionsAlder - alder);
             }
 
-            Console.ReadLine();     // To prevent the console to close after the program is done. 
+            Console.ReadLine();     // To prevent the console to close after the program is done
         }
     }
 }
